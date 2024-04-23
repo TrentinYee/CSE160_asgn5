2b4d30e1026aa7ff5e0b95c2063b928bcbb9211d
Trentin <trryee@ucsc.edu>
2024-04-23 04:21:54 -0700

    should be done for now, added the red and blue, temporary spinning foxbox, and a cocktoo because they had no chickens

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CSE160_asgn5
 Three.js scene making
+
+bird model found here: https://free3d.com/3d-model/bird-v1--19886.html
+thank you for making a model with a functioning texture
--- main.js
+++ main.js
@@ -1,34 +1,98 @@
 import * as THREE from 'three';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 
 function main() {
 
 	const canvas = document.querySelector( '#c' );
 	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
 
-	const fov = 75;
-	const aspect = 2; // the canvas default
+	const fov = 90;
+	const aspect = 4; // the canvas default
 	const near = 0.1;
-	const far = 5;
+	const far = 500;
 	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.z = 2;
+	
+    // camera
+    camera.position.x = 5;
+    camera.position.y = 10;
+    camera.position.z = 30;
+    camera.lookAt(0, 2, 0);
 
 	const scene = new THREE.Scene();
-
+    scene.background = new THREE.Color('black');
 	{
 
 		const color = 0xFFFFFF;
 		const intensity = 3;
 		const light = new THREE.DirectionalLight( color, intensity );
-		light.position.set( - 1, 2, 4 );
+		light.position.set( 0, 0, 10 );
 		scene.add( light );
 
 	}
 
-	const boxDimensions = [1, 1, 1];
-    const sphereDimensions = [1, 32, 16];
-	const cube_geometry = new THREE.BoxGeometry( boxDimensions[0], boxDimensions[1], boxDimensions[2] );
-    const ball_geometry = new THREE.SphereGeometry( sphereDimensions[0], sphereDimensions[1], sphereDimensions[2]);
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+          renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
 
+    // primary shape dimensions and geometry
+	const boxDimensions = [10, 10, 10];
+    const sphereDimensions = [10, 32, 32];
+	const boxGeometry = new THREE.BoxGeometry( boxDimensions[0], boxDimensions[1], boxDimensions[2] );
+    const ballGeometry = new THREE.SphereGeometry( sphereDimensions[0], sphereDimensions[1], sphereDimensions[2]);
+
+    // texture stuff
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load( 'fox.png' );
+    texture.colorSpace = THREE.SRGBColorSpace;
+
+    // foxbox
+    const FoxMaterial = new THREE.MeshBasicMaterial({
+        map: texture,
+    });
+
+    const foxbox = new THREE.Mesh(boxGeometry, FoxMaterial);
+    scene.add(foxbox);
+    foxbox.position.x = -10;
+    foxbox.position.y = 0;
+    foxbox.position.z = 25;
+
+    // chimkin
+    const mtlLoader = new MTLLoader();
+    mtlLoader.load( 'Bird_v1_L2/Bird_v1/12259_Bird_v1_L2.mtl', ( mtl ) => {
+
+        mtl.preload();
+        const objLoader = new OBJLoader();
+        objLoader.setMaterials( mtl );
+        objLoader.load( 'Bird_v1_L2/Bird_v1/12259_Bird_v1_L2.obj', ( root ) => {
+
+            scene.add( root );
+            root.scale.set(0.5, 0.5, 0.5);
+
+            root.rotation.x = 3* Math.PI / 2;
+
+            root.position.x = 0; // put on level with foxbox when camera controls are added, this is just for the shot
+            root.position.y = 0;
+            root.position.z = 0;
+
+
+        } );
+
+    } );
+    
+    // chimkin mtrl file
+    
+
+
+
+    // makes an untextured shape given all the parameters
 	function makeInstance( geometry, color, x, y, z) {
 
 		const material = new THREE.MeshPhongMaterial( { color } );
@@ -45,14 +109,20 @@ function main() {
 	}
 
 	const shapes = [
-		makeInstance( cube_geometry, 0x44aa88, 0, 0, 0),
-		makeInstance( sphere_geometry, 0x730811, - 2, 0, 0),
-		makeInstance( sphere_geometry, 0x184496, 2, 0, 0),
+		foxbox,
+		makeInstance( ballGeometry, 0x730811, - 20, 10, -25),
+		makeInstance( ballGeometry, 0x184496, 20, 10, -25),
 	];
 
 	function render( time ) {
 
 		time *= 0.001; // convert time to seconds
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
 
 		shapes.forEach( ( cube, ndx ) => {
 
