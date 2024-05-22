bad451ffada0eb8e6e0cbdce036e621633cf5e14
Trentin <trryee@ucsc.edu>
2024-05-22 00:34:07 -0700

    added lights, camera, and sky (not action)

--- main.js
+++ main.js
@@ -1,33 +1,68 @@
 import * as THREE from 'three';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+import { OrbitControls} from 'three/addons/controls/OrbitControls.js'
 
 function main() {
 
 	const canvas = document.querySelector( '#c' );
 	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
 
-	const fov = 90;
-	const aspect = 4; // the canvas default
+	const fov = 60;
+	const aspect = 2; // the canvas default
 	const near = 0.1;
-	const far = 500;
-	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+	const far = 1000;
 	
-    // camera
-    camera.position.x = 5;
-    camera.position.y = 10;
-    camera.position.z = 30;
-    camera.lookAt(0, 2, 0);
+	
+    // camera1
+    const camera1 = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera1.position.x = 5;
+    camera1.position.y = 10;
+    camera1.position.z = 42;
+    camera1.lookAt(0, 2, 0);
+
+    // camera2
+    const camera2 = new THREE.PerspectiveCamera( fov, aspect, near, far);
+      camera2.position.set(40, 10, 30);
+      camera2.lookAt(0, 5, 0);
+    
+      // orbit controls
+      const oControls = new OrbitControls(camera2, canvas);
+      oControls.target.set(0, 5, 0);
+      oControls.update();
+
+
+    // scene setup
 
 	const scene = new THREE.Scene();
     scene.background = new THREE.Color('black');
 	{
-
+        // Ambient Light
+        const ambColor = 0x642091;
+        const ambIntensity = 0.1;
+        const ambLight = new THREE.AmbientLight(ambColor, ambIntensity);
+        scene.add(ambLight);
+        
+        // Directional Light
 		const color = 0xFFFFFF;
-		const intensity = 3;
+		const intensity = 0.5;
 		const light = new THREE.DirectionalLight( color, intensity );
 		light.position.set( 0, 0, 10 );
 		scene.add( light );
+
+        // Point Light 1
+        const p1color = 0xff0000;
+		const p1intensity = 1000;
+		const p1light = new THREE.PointLight( p1color, p1intensity );
+		p1light.position.set( -30, 10, -5 );
+		scene.add( p1light );
+
+        // Point Light 2
+        const p2color = 0x0000ff;
+		const p2intensity = 1000;
+		const p2light = new THREE.PointLight( p2color, p2intensity );
+		p2light.position.set( 10, 10, -5 );
+		scene.add( p2light );
 
 	}
 
@@ -42,27 +77,64 @@ function main() {
         return needResize;
     }
 
+    // taken straight from three.js manual, loads textures given path
+    function loadColorTexture( path ) {
+        const texture = loader.load( path );
+        texture.colorSpace = THREE.SRGBColorSpace;
+        return texture;
+    }
+
     // primary shape dimensions and geometry
 	const boxDimensions = [10, 10, 10];
     const sphereDimensions = [10, 32, 32];
 	const boxGeometry = new THREE.BoxGeometry( boxDimensions[0], boxDimensions[1], boxDimensions[2] );
     const ballGeometry = new THREE.SphereGeometry( sphereDimensions[0], sphereDimensions[1], sphereDimensions[2]);
-
-    // texture stuff
+    const roadsize = 50;
+    
+    // texture defining
     const loader = new THREE.TextureLoader();
-    const texture = loader.load( 'textures/fox.png' );
-    texture.colorSpace = THREE.SRGBColorSpace;
+    const foxtexture = loadColorTexture( 'textures/fox.png' );
+    const roadtexture = loadColorTexture( 'textures/road-texture.jpg' );
+    roadtexture.wrapS = THREE.RepeatWrapping;
+    roadtexture.wrapT = THREE.RepeatWrapping;
+    roadtexture.magFilter = THREE.NearestFilter;
+    const repeats = roadsize/2;
+    roadtexture.repeat.set(repeats, repeats);
 
-    // foxbox
-    const FoxMaterial = new THREE.MeshBasicMaterial({
-        map: texture,
+    // skybox texture
+    const skyloader = new THREE.TextureLoader();
+    const skytexture = skyloader.load(
+        'textures/aristea_wreck_puresky.jpg',
+        () => {
+        skytexture.mapping = THREE.EquirectangularReflectionMapping;
+        skytexture.colorSpace = THREE.SRGBColorSpace;
+        scene.background = skytexture;
     });
 
-    const foxbox = new THREE.Mesh(boxGeometry, FoxMaterial);
-    scene.add(foxbox);
+    // foxbox
+    const materials = [
+        new THREE.MeshBasicMaterial({map: foxtexture}),
+        new THREE.MeshBasicMaterial({map: roadtexture}),
+    ];
+
+    const foxbox = new THREE.Mesh(boxGeometry, materials[0]);
+    scene.add(foxbox); //adds shape to scene to be rendered
     foxbox.position.x = -10;
-    foxbox.position.y = 0;
-    foxbox.position.z = 25;
+    foxbox.position.y = 7.5;
+    foxbox.position.z = 55;
+
+    // roadplate
+    const road = new THREE.Mesh(boxGeometry, materials[1]);
+    scene.add(road);
+    road.position.x = 0;
+    road.position.y = -5;
+    road.position.z = 100;
+    road.scale.set(roadsize, 0.5, roadsize);
+
+    // other shapes ---------------------------------
+    
+    // floor
+    const floorPlane = makeInstance( boxGeometry, 0x175e1e, [0, -18, 0], [1000, 0, 1000]);
 
     // chimkin
     const mtlLoader = new MTLLoader();
@@ -76,42 +148,42 @@ function main() {
             scene.add( root );
             root.scale.set(0.5, 0.5, 0.5);
 
+            root.position.x = -10; // put on level with foxbox when camera controls are added, this is just for the shot
+            root.position.y = -2.5;
+            root.position.z = -10;
+
             root.rotation.x = 3* Math.PI / 2;
 
-            root.position.x = 0; // put on level with foxbox when camera controls are added, this is just for the shot
-            root.position.y = 0;
-            root.position.z = 0;
-
+    
 
         } );
 
     } );
     
-    // chimkin mtrl file
-    
-
-
 
     // makes an untextured shape given all the parameters
-	function makeInstance( geometry, color, x, y, z) {
+	function makeInstance( geometry, color, position, scale) {
 
 		const material = new THREE.MeshPhongMaterial( { color } );
 
 		const shape = new THREE.Mesh( geometry, material );
 		scene.add( shape );
 
-		shape.position.x = x;
-        shape.position.y = y;
-        shape.position.z = z;
+		shape.position.x = position[0];
+        shape.position.y = position[1];
+        shape.position.z = position[2];
+
+        shape.scale.set(scale[0], scale[1], scale[2]);
 
 		return shape;
 
 	}
 
-	const shapes = [
+    // contains all the shapes to be animated
+	const spinshapes = [
 		foxbox,
-		makeInstance( ballGeometry, 0x730811, - 20, 10, -25),
-		makeInstance( ballGeometry, 0x184496, 20, 10, -25),
+		makeInstance( ballGeometry, 0x730811, [-30, 10, -25], [1, 1, 1]),
+		makeInstance( ballGeometry, 0x184496, [10, 10, -25], [1, 1, 1]),
 	];
 
 	function render( time ) {
@@ -120,11 +192,11 @@ function main() {
 
         if (resizeRendererToDisplaySize(renderer)) {
             const canvas = renderer.domElement;
-            camera.aspect = canvas.clientWidth / canvas.clientHeight;
-            camera.updateProjectionMatrix();
+            camera2.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera2.updateProjectionMatrix();
         }
 
-		shapes.forEach( ( cube, ndx ) => {
+		spinshapes.forEach( ( cube, ndx ) => {
 
 			const speed = 1 + ndx * .1;
 			const rot = time * speed;
@@ -133,7 +205,7 @@ function main() {
 
 		} );
 
-		renderer.render( scene, camera );
+		renderer.render( scene, camera2 );
 
 		requestAnimationFrame( render );
 
